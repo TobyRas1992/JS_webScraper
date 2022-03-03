380621e76532e7c15d13f51e039b3e57c47241e1
Tobias Rasmussen <tobiasrasmussen@outlook.com>
2022-03-03 12:51:56 +0100

    comments added

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const PORT = 8000;
-const axios = require("axios");
-const cheerio = require("cheerio");
-const express = require("express");
+const axios = require("axios"); // handles data transfers/retrievals
+const cheerio = require("cheerio"); // like jQuery for traversing HTML files
+const express = require("express"); //node framework
 
-const app = express();
+const app = express(); //framework for our server
 
-const url = "https://www.theguardian.com/international";
-axios(url)
+const url = "https://www.theguardian.com/international"; //update to chosen website
+axios(url) // data retrieval
   .then((response) => {
     const html = response.data;
     const x = cheerio.load(html);
     const articles = [];
+    //change function below to match html elements on new site
+    //Below we use cheerio to traverse html elements on retrieved site
     x(".fc-item__title", html).each(function () {
       const title = x(this).text();
       const url = x(this).find("a").attr("href");
